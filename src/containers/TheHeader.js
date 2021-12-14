161b7386318fb304d232aad107381093dfb34095
Farizio Kautsar Heruzy <kheruzy@gmail.com>
2021-12-14 22:53:59 +0700

    Header simplify

--- src/containers/TheHeader.js
+++ src/containers/TheHeader.js
@@ -55,17 +55,7 @@ const TheHeader = () => {
         <CIcon name="logo" height="48" alt="Logo"/>
       </CHeaderBrand>
 
-      <CHeaderNav className="d-md-down-none mr-auto">
-        <CHeaderNavItem className="px-3" >
-          <CHeaderNavLink to="/dashboard">Dashboard</CHeaderNavLink>
-        </CHeaderNavItem>
-        <CHeaderNavItem  className="px-3">
-          <CHeaderNavLink to="/users">Users</CHeaderNavLink>
-        </CHeaderNavItem>
-        <CHeaderNavItem className="px-3">
-          <CHeaderNavLink>Settings</CHeaderNavLink>
-        </CHeaderNavItem>
-      </CHeaderNav>
+      <CBreadcrumbRouter className="border-0 c-subheader-nav m-0 px-0 px-md-3 mr-auto" routes={routes} />
 
       <CHeaderNav className="px-3">
         <CToggler
@@ -77,37 +67,18 @@ const TheHeader = () => {
           <CIcon name="cil-moon" className="c-d-dark-none" alt="CoreUI Icons Moon" />
           <CIcon name="cil-sun" className="c-d-default-none" alt="CoreUI Icons Sun" />
         </CToggler>
-        <TheHeaderDropdownNotif/>
+        {/* <TheHeaderDropdownNotif/>
         <TheHeaderDropdownTasks/>
-        <TheHeaderDropdownMssg/>
+        <TheHeaderDropdownMssg/> */}
         <TheHeaderDropdown/>
-        <CToggler
+        {/* <CToggler
           inHeader
           className="d-md-down-none"
           onClick={() => dispatch({type: 'set', asideShow: !asideShow})}
         >
           <CIcon className="mr-2" size="lg" name="cil-applications-settings" />
-        </CToggler>
+        </CToggler> */}
       </CHeaderNav>
-
-      <CSubheader className="px-3 justify-content-between">
-        <CBreadcrumbRouter className="border-0 c-subheader-nav m-0 px-0 px-md-3" routes={routes} />
-          <div className="d-md-down-none mfe-2 c-subheader-nav">
-            <CLink className="c-subheader-nav-link"href="#">
-              <CIcon name="cil-speech" alt="Settings" />
-            </CLink>
-            <CLink
-              className="c-subheader-nav-link"
-              aria-current="page"
-              to="/dashboard"
-            >
-              <CIcon name="cil-graph" alt="Dashboard" />&nbsp;Dashboard
-            </CLink>
-            <CLink className="c-subheader-nav-link" href="#">
-              <CIcon name="cil-settings" alt="Settings" />&nbsp;Settings
-            </CLink>
-          </div>
-      </CSubheader>
     </CHeader>
   )
 }
